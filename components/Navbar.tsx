dae5294561fb4245329ca29d551391967bb2c2ff
Qurux Dev <info@qurux.in>
2026-09-09 16:24:32 +0530

    Navbar: reduce left padding to move logo closer to left edge

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 bg-white shadow-sm">
 
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-6">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-2 sm:px-6">
 
         {/* Logo */}
         <Link href="/" className="flex items-center">
